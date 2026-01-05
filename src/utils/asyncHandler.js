0ca7160270317203f28bb45285ddaffe05a0eb07
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-01-05 21:02:18 +0530

    implement asyncHandler utility using promises for error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//        res.status(500).json({ message: error.message });
+        
+//     }
+// }
+
+//using promise
+
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).
+    catch((error)=>next(error));
+}
+
+export default asyncHandler;
