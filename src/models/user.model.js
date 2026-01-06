ce7f4cb532da009d91f2ca59863cc9539383afb2
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-01-06 23:20:39 +0530

    add user model with schema definition for user data

---
+++ src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose , {Schema} from "mongoose";
+
+const userSchema = new Schema({
+   username:{
+        type:String,    
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        index:true
+   } ,
+   email:{
+        type:String,    
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+   },
+   fullname:{
+        type:String,    
+        required:true,
+        trim:true,
+        index:true
+   } ,
+   avatar:{
+        type:String, //cloudinary url   
+        trim:true,
+        required:true,
+   },
+   coverimage:{
+        type:String, //cloudinary url   
+   },
+   watchhistory:[
+    {
+        type:Schema.Types.ObjectId,
+        ref:"Video"
+    }
+   ],
+   password:{
+        type:String,    
+        required:[true,'Password is required'],
+   },
+   refreshTokens:[
+    {
+        type:String,
+    }
+   ]
+},
+{timestamps:true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
