2f277fb56db67b7548b8cc81fc09b9923ca794c6
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-02-13 23:15:06 +0530

    refactor cloudinary utility: streamline configuration and remove unused code

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,42 +1,20 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
-(async function() {
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+export const uploadToCloudinary = async (filePath, folder = "uploads") => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto", // handles image, video, pdf, etc
+      folder,
     });
-    
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
