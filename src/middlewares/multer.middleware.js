7981b40f92e8182dbfde2fc9543169145572a178
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-02-13 23:23:57 +0530

    add multer middleware for file uploads: configure storage, file filtering, and ensure uploads directory exists

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,35 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+// ensure uploads folder exists
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const uniqueName = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${ext}`;
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // allow everything (image, video, pdf, audio, etc.)
+  cb(null, true);
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 50 * 1024 * 1024, // 50 MB
+  },
+});
