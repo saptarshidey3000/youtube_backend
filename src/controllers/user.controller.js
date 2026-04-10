9640b49e4f03a8f8c5b66acc92776eb5480be325
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-04-10 21:06:52 +0530

    add detailed comments to registerUser controller for clarity

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,15 @@ import asyncHandler from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-
+    //get user details from request body
+    //valdation - not empty 
+    // check if user already exists : username or email
+    //check for image , check for avatar 
+    //upload image to cloudinary and get url
+    //create user in database - create entry in db 
+    //remove password and refresh token field from response 
+    //check for user creation success and send response
+    //return res 
 });
 
 export { registerUser };
