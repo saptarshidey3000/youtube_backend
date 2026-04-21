06e3e44e6e15414365be87164a5522cd761f5df8
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-04-21 16:28:35 +0530

    enhance JWT verification error handling and structure

--- src/middlewares/auth.middlewar.js
+++ src/middlewares/auth.middlewar.js
@@ -5,12 +5,30 @@ import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) 
 => {
-    const token = req.cookies?.accessToken || req.headers
-        ("Authorization")?.replace("Bearer ", "") 
-
-        if (!token) {
-            throw new ApiError(401, "Unauthorized: No token provided");
+    try {
+        const token = req.cookies?.accessToken || req.headers
+            ("Authorization")?.replace("Bearer ", "") 
+    
+            if (!token) {
+                throw new ApiError(401, "Unauthorized: No token provided");
+            }
+            
+        const decodedToken = await jwt.verify(token, process.env.
+            ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById
+        (decodedToken?._id).select("-password -refreshToken");
+    
+        if (!user) {
+            throw new ApiError(401, "Unauthorized: User not found");
         }
-        
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error.message || "Unauthorized"); 
+    }
+
 });
+
 
